5ff8cc18ccb3c9eed642a12f5ec7a0ca5610a168
maximus-z <39885533+maximus-z@users.noreply.github.com>
2018-10-05 19:25:10 -0400

    Delete fact_population_constraint.sql

--- fact_population_constraint.sql
+++
@@ -1,2 +0,0 @@
-alter table fact_population add constraint fact_population_fk1 foreign key (state_sk) references dim_state(state_sk);
-alter table fact_population add constraint fact_population_fk2 foreign key (county_sk) references dim_county(county_sk);
